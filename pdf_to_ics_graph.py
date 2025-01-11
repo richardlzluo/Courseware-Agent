fae4c3819536150307b6d08a4dd9cab6124ba8b8
richardlzluo <richard.lz.luo@gmail.com>
2025-01-11 12:02:26 -0500

    Initial commit

---
+++ pdf_to_ics_graph.py
@@ -0,0 +1,107 @@
+
+from config import Config
+import base64
+import io
+import fitz
+from PIL import Image
+import os
+
+import datetime
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langgraph.graph import MessagesState, START, END, StateGraph
+from langsmith import traceable
+
+
+from pydantic import BaseModel, Field 
+from typing import List, Optional
+
+from icalendar import Calendar, Event
+
+
+def pdf_to_base64_images(pdf_path: str):
+    pdf_document = fitz.open(pdf_path)
+    images_base64 = []
+    
+    for page_number in range(len(pdf_document)):  # Iterate through all pages
+        page = pdf_document.load_page(page_number)
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        images_base64.append(base64.b64encode(buffer.getvalue()).decode("utf-8"))
+    
+    pdf_document.close()
+    return images_base64
+
+
+title_description = """
+The format should contain the following informations:
+1.The course code
+2.The assignment type
+3.The assignment number(if any, the number should start with a #)
+
+For example:
+TIMG 5003 -- Assignment #2
+TIMG 5303 -- Exam #1A
+TIMG 5103 -- Quiz
+ITEC 3903 -- Take Home Exam
+SCMP 5409B -- Final Exam 
+"""
+
+class ScheduleEntry(BaseModel):
+    title: str = Field(description=title_description)
+    description: str = Field(description="A brief description of the schedule entry in no more than five sentences")
+    date: datetime.date = Field(description="The date of the schedule entry.")
+    time: datetime.time = Field(description="The time of the schedule entry. If no time is specified in the document, just use 23:59:59 as the time.")
+
+class Schedule(BaseModel):
+    has_schedule: bool = Field(description="Does the document have a schedule?")
+    follow_up: str = Field(description="Follow up message to inform the user to upload another file.")
+    schedule: List[ScheduleEntry] = Field(description="A list of schedule entries.")
+
+llm = ChatOpenAI(model=Config.chatGPT_model, temperature=0) 
+
+SYSTEM_PROMPT = """
+You will be given a document that is the outline of a course. 
+Read through the document and extract the submission time of each assignment, exam, homework, or something like these. 
+Try your best to extract the title, the specific tasks, and the most importantly, the date and time of the submission time. 
+The submission time should be returned as a date and a time seperately.
+Fill the schedule property in to output as true and generate a list of schedule entries. 
+If the whole schedule is not available, just fill the has_schedule property in to output as false and generate a follow up message to inform the user to upload another file and fill it in the follow_up property. 
+"""
+
+@traceable
+def extract_calendar(state: MessagesState):
+    generation_instructions = SystemMessage(content=SYSTEM_PROMPT)
+    messages = [generation_instructions, state["messages"][-1]]
+    structured_llm = llm.with_structured_output(Schedule)
+    result = structured_llm.invoke(messages)
+    return {"messages": AIMessage(content=[{"result": result}])}
+
+
+
+def get_graph():
+    builder = StateGraph(MessagesState)
+    builder.add_node("extract_calendar", extract_calendar)
+    builder.add_edge(START, "extract_calendar")
+    builder.add_edge("extract_calendar", END)
+    graph = builder.compile()
+
+    print("graph compiled") 
+    return graph
+
+def get_calendar(schedule):
+    cal = Calendar()
+    cal.add('prodid', '-//Richard Luo//Courseware-to-ICS//')
+    for schedule_entry in schedule:
+        event = Event()
+        event.add('summary', schedule_entry.title)
+        event.add('description', schedule_entry.description)
+        event.add('dtstart', datetime.datetime.combine(schedule_entry.date, schedule_entry.time))
+        event.add('dtend', datetime.datetime.combine(schedule_entry.date, schedule_entry.time))
+        event.add('dtstamp', datetime.datetime.now())
+        cal.add_component(event)
+    
+    return cal
---
+++ telegram_bot_run.py
@@ -0,0 +1,99 @@
+
+from config import Config
+
+import logging
+from telegram import Update
+from telegram.constants import ChatAction
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
+import uuid
+import time
+from pdf_to_ics_graph import *
+import base64
+from PIL import Image
+from io import BytesIO
+
+graph = get_graph()
+
+def show_images(base64_images):
+    for base64_image in base64_images:
+        image_data = base64.b64decode(base64_image)
+        image = Image.open(BytesIO(image_data))
+        image.show()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+greeting = """
+Hi there! I'm here to help you generate a calendar for your assignments and exams from yor course outline. Please send me your course outline as a PDF file.
+<u>Be cautious</u>:
+- I just accept PDF files, don't send me anything else.
+- Currently, I can only finish this single task, don't even try to instruct me to do anything else. I won't response. 
+- The calendar is generated using an LLM. I can't guarantee the accuracy of the calendar. Please double check it yourself.
+"""
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=greeting)
+    
+async def handle_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id
+    """Handle PDF file uploads."""
+    file = update.message.document
+    if file.mime_type == 'application/pdf':
+        file_id = file.file_id
+        file_name = file.file_name
+
+        #await update.message.reply_text(f"Received your PDF: {file_name}")
+        
+        # Optionally, download the file
+        new_file = await context.bot.get_file(file_id)
+        new_file_name = f"file_{int(time.time())}_{uuid.uuid4().hex}.pdf"
+        await new_file.download_to_drive(f"./cache/{new_file_name}")
+        await update.message.reply_text(f"Your PDF file has been accepted. Please give me a second to process it⏳...")
+        await context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+        
+        
+        base64_images = pdf_to_base64_images(Config.cache_dir+new_file_name)
+        os.remove(Config.cache_dir+new_file_name)
+        messages = [HumanMessage(
+                        content=[
+                            {
+                                "type": "image_url",
+                                "image_url": {"url": f"data:image/jpeg;base64,{image}"}
+                            } for image in base64_images
+                        ]
+                    )]
+
+        result = graph.invoke({"messages": messages})
+        
+        if result["messages"][-1].content[0]['result'].has_schedule:
+            schedule = result["messages"][-1].content[0]['result'].schedule
+            cal = get_calendar(schedule)
+            ics_file_name = f"assignment_schedule_{int(time.time())}_{uuid.uuid4().hex}.ics"
+            with open(Config.cache_dir+ics_file_name, 'wb') as f:
+                f.write(cal.to_ical())
+                
+            try:
+                # Send the file
+                await context.bot.send_document(chat_id=chat_id, document=open(Config.cache_dir+ics_file_name, 'rb'))
+                await update.message.reply_text("Here is the schedule file!")
+            except FileNotFoundError:
+                await update.message.reply_text("File not found. Please check the file path.")
+            except Exception as e:
+                await update.message.reply_text(f"An error occurred: {e}")    
+        else:
+            await update.message.reply_text("This file does have a schedule for assignments or exams on it. Please try another one.")
+                
+    else:
+        await update.message.reply_text("The file you sent is not a PDF. Please send a valid PDF file.")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(Config.telegram_bot_token).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    application.add_handler(MessageHandler(filters.Document.MimeType("application/pdf"), handle_pdf))
+    
+    application.run_polling()
+    
+    
