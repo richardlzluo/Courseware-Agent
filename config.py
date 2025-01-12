7c8946b07302f2c7be8891d7741db32a180a6e81
richardlzluo <richard.lz.luo@gmail.com>
2025-01-12 17:21:10 -0500

    update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config():
     # Replace with your bot token and channel ID
     telegram_bot_token = 'REPLACE YOU TELEGRAM BOT TOKEN'
-    telegram_channel_id = 'REPLACE YOU TELEGRAM CHANNEL ID'  )
+    telegram_channel_id = 'REPLACE YOU TELEGRAM CHANNEL ID'
     chatGPT_model = 'gpt-4o-mini'
     
     cache_dir = './cache/'
