fe1b6d9dd52d83ba7df93e859c57a61ae1373042
richardlzluo <60744867+richardlzluo@users.noreply.github.com>
2025-01-13 22:40:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 class Config():
     # Replace with your bot token and channel ID
-    telegram_bot_token = 'REPLACE YOU TELEGRAM BOT TOKEN'
-    telegram_channel_id = 'REPLACE YOU TELEGRAM CHANNEL ID'
+    telegram_bot_token = 'YOUR TELEGRAM BOT TOKEN'
+    telegram_channel_id = 'YOUR TELEGRAM CHANNEL ID'
     chatGPT_model = 'gpt-4o-mini'
     
     cache_dir = './cache/'
     working_dir = './working/'
 
-os.environ["OPENAI_API_KEY"] = "REPLACE YOU OPENAI API KEY"
+os.environ["OPENAI_API_KEY"] = "YOUR OPENAI API KEY"
